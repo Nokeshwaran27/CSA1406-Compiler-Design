1ef8a32f27a7cd0c819cc77d0c5cfa16ba830742
Nokeshwaran27 <192511007.simats@saveetha.com>
2026-08-06 14:26:30 +0530

    Add files via upload

---
+++ Lab Experiment 2/EXPT 2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[500];
+
+    printf("Enter a line: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Check for single-line comment
+    if (str[0] == '/' && str[1] == '/') {
+        printf("Single-line Comment\n");
+    }
+
+    // Check for multi-line comment
+    else if (str[0] == '/' && str[1] == '*') {
+        int len = strlen(str);
+
+        if (len >= 4 && str[len-3] == '*' && str[len-2] == '/')
+            printf("Multi-line Comment\n");
+        else
+            printf("Invalid Multi-line Comment\n");
+    }
+
+    // Not a comment
+    else {
+        printf("Not a Comment\n");
+    }
+
+    return 0;
+}
